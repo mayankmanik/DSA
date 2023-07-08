f7c81346a1e9bf2c87019ea1f1ee21f65cbcbc93
Mayank Manik <mayank18jha@gmail.com>
2023-07-08 19:32:23 +0530

    connected components

---
+++ Graphs/2.connected_components.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<int> vis(n+1,0);
+    for(int i=1;i<n;i++)
+    {
+        if(!vis[i])
+        {
+            // traversal(i);
+        }
+    }
+    return 0;
+}
