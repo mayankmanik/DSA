6dbcd10ca7a67d155fe64b58461ffa03deee0d03
Mayank Manik <mayank18jha@gmail.com>
2023-07-09 13:49:42 +0530

    disjoint set

---
+++ Graphs/19.disjoint_set.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet
+{
+private:
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        for (int i = 0; i <= n; i++)
+            parent[i] = i;
+        size.resize(n + 1, 1);
+    }
+
+    int findPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findPar(u);
+        int ulp_v = findPar(v);
+
+        if (ulp_u == ulp_v)
+            return;
+        //  attach smaller size to larger size
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findPar(u);
+        int ulp_v = findPar(v);
+
+        if (ulp_u == ulp_v)
+            return;
+        //  attach smaller rank to larger rank
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
