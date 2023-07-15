d3aee748806609b4ae9fbb1f3c2cae4fa54fa297
Mayank Manik <mayank18jha@gmail.com>
2023-07-15 16:45:49 +0530

    maximum width

---
+++ Trees/21.maximum_width_of_binary_tree.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int width(Node *root)
+{
+    if (!root)
+        return 0;
+    int ans = 0;
+    queue<pair<Node *, int>> q;
+    q.push({root, 0});
+
+    while (!q.empty())
+    {
+        int size = q.size();
+        int mmin = q.front().second;
+        int first, last;
+        for (int i = 0; i < size; i++)
+        {
+            int cur_id = q.front().second - mmin;
+            auto node = q.front().first;
+            q.pop();
+            if (i == 0)
+                first = cur_id;
+            if (i == size - 1)
+                last = cur_id;
+            if (node->left)
+                q.push({node->left, cur_id * 2 + 1});
+            if (node->right)
+                q.push({node->right, cur_id * 2 + 2});
+        }
+        ans = max(ans, last - first + 1);
+    }
+    return ans;
+}
+int main()
+{
+
+    return 0;
+}
